246e88c88efaea4cdb1bcd30b1f69275c6bc7856
Эльмира <yndx-garmagelm@yandex.ru>
2021-05-17 23:29:29 +0300

    My hw v.1

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -93,7 +93,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework_simplejwt.authentication.JWTAuthentication',  # noqa
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # noqa
     ],
 }
 
